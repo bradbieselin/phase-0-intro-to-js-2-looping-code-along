9d4c437515a1588a6710e34a50b94dcc01407cfe
Brad Bieselin <bradley.bieselin@gmail.com>
2022-04-05 16:31:43 -0400

    commit tree 0df28c8d35ba38c514f7af343239968a4d0b526b

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solutions in this file
+const writeCards = (names, eventName) => {
+    for(let i = 0; i < names.length; i++) {
+        const newArray = [];
+        newArray.pop(names[i]);
+        return newArray;
+    }
+}
